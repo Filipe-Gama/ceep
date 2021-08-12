6c02dfd438fb35e32e366ad21e9232112c24201f
Filipe Gama <filipegama715@gmail.com>
2021-08-12 16:54:30 -0300

    Deletar cards

--- src/components/CardNota/CardNotas.jsx
+++ src/components/CardNota/CardNotas.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import "./estilo.css";
+import { ReactComponent as DeleteSVG } from "../../assets/img/delete.svg";
 
 class CardNotas extends Component {
+  apagar() {
+    const indice = this.props.indice;
+    this.props.apagarNota(indice);
+  }
+
   render() {
     return (
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
         </header>
+        <DeleteSVG onClick={this.apagar.bind(this)} />
         <p className="card-nota_texto">{this.props.texto}</p>
       </section>
     );
